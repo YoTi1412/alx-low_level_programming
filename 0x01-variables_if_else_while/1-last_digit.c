b2ba3863efbdd0187d87477d9cfaa4cd0d35b478
Yasser Rafai <yasserrafai5@gmail.com>
2023-03-17 08:58:29 -0700

    task 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Task one
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n, last_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	last_digit = n % 10;
+
+	printf("Last digit of %d is %d", n, last_digit);
+
+	if (last_digit > 5)
+	{
+		puts(" and is greater than 5");
+	}
+	else if (last_digit == 0)
+	{
+		puts(" and is 0");
+	}
+	else
+	{
+		puts(" and is less than 6 and not 0");
+	}
+
+	return (0);
+}
