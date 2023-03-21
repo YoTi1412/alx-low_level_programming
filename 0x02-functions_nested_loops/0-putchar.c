454dc41fa5e1b244565aea6860812bcf269c9b35
Yasser Rafai <yasserrafai5@gmail.com>
2023-03-21 11:12:18 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,30 +1,22 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+ * main - prints _putchar, followed by a new line.
+ * Return: 0
+*/
+
 int main(void)
 {
-    char c = '_';
-    write(1, &c, 1);
-    c = 'p';
-    write(1, &c, 1);
-    c = 'u';
-    write(1, &c, 1);
-    c = 't';
-    write(1, &c, 1);
-    c = 'c';
-    write(1, &c, 1);
-    c = 'h';
-    write(1, &c, 1);
-    c = 'a';
-    write(1, &c, 1);
-    c = 'r';
-    write(1, &c, 1);
-    c = '\n';
-    write(1, &c, 1);
+	unsigned int i;
+	char c[] = "_putchar";
 
-    return (0);
+	for (i = 0; i <= 7; i++)
+	{
+		_putchar(c[i]);
+	}
+
+	_putchar('\n');
+
+	return (0);
 }
+
