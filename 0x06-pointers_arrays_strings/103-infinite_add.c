e47a6d96703ff3c5adca81dec80d72727111e78b
Yasser Rafai <yasserrafai5@gmail.com>
2023-03-29 16:59:37 -0700

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,7 +8,7 @@
  * @size_r: buffer size
  * ahhh! Crazy task! Expand your knowledge
  * Return: address of r or 0
- */
+*/
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i, j, k, l, m, n;
