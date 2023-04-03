fd47ace4ed534b3cc98828a6f19ba4994e9f48ff
Yasser Rafai <yasserrafai5@gmail.com>
2023-04-03 16:27:59 -0700

    task 8

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,25 +1,10 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * set_string - function that  sets the value of a pointer to a char.
+ *@s: first value -char
+ *@to: second value -int
 */
-
 void set_string(char **s, char *to)
 {
-    *s = to;
-}
-
-
-int main(void)
-{
-    char *s0 = "Bob Dylan";
-    char *s1 = "Robert Allen";
-
-    printf("%s, %s\n", s0, s1);
-    set_string(&s1, s0);
-    printf("%s, %s\n", s0, s1);
-    return (0);
+	*s = to;
 }
